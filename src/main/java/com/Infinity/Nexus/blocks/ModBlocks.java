d0f81e350dce392c70edb5d0325620dd679bca2a
Reaviik <reavik_gamer@hotmail.com>
2023-09-02 12:42:56 -0300

    First Commit

--- src/main/java/com/Infinity/Nexus/blocks/ModBlocks.java
+++ src/main/java/com/Infinity/Nexus/blocks/ModBlocks.java
@@ -1,10 +1,13 @@
 package com.Infinity.Nexus.blocks;
 
 import com.Infinity.Nexus.InfinityNexus;
+import com.Infinity.Nexus.Tab.ModTabs;
+import com.Infinity.Nexus.blocks.custom.FlatLight;
 import com.Infinity.Nexus.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -19,19 +22,42 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, InfinityNexus.MOD_ID);
 
-    public static final RegistryObject<Block> MACHINE_FRAME = registerBlock("machine_frame",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+    //Custom
+    public static final RegistryObject<Block> FLAT_LIGHT = registerBlock("flat_light",
+            () -> new FlatLight(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(3f).lightLevel(LUZ -> {return 12;})), ModTabs.INFINITY_NEXUS,Rarity.COMMON);
+
+    public static final RegistryObject<Block> COPPER_MACHINE_CASING = registerBlock("copper_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(9f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.COMMON);
+    public static final RegistryObject<Block> ALUMINUM_MACHINE_CASING = registerBlock("aluminum_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(3f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.COMMON);
+    public static final RegistryObject<Block> IRON_MACHINE_CASING = registerBlock("iron_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(6f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.RARE);
+    public static final RegistryObject<Block> GOLD_MACHINE_CASING = registerBlock("gold_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(6f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.RARE);
+    public static final RegistryObject<Block> STEEL_MACHINE_CASING = registerBlock("steel_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(6f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.UNCOMMON);
+    public static final RegistryObject<Block> MAGIC_MACHINE_CASING = registerBlock("magic_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(9f).lightLevel(LUZ -> {return 12;})), ModTabs.INFINITY_NEXUS, Rarity.UNCOMMON);
+    public static final RegistryObject<Block> INDUSTRIAL_MACHINE_CASING = registerBlock("industrial_machine_casing",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()
+                    .strength(7f).lightLevel(LUZ -> {return 2;})), ModTabs.INFINITY_NEXUS, Rarity.EPIC);
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab,Rarity rarity) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn, tab);
+        registerBlockItem(name, toReturn, tab, rarity);
         return toReturn;
     }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
-                                                                            CreativeModeTab tab) {
+                                                                            CreativeModeTab tab, Rarity rarity) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(tab)));
+                new Item.Properties().tab(tab).rarity(rarity)));
     }
 
     public static void register(IEventBus eventBus) {
--- src/main/java/com/Infinity/Nexus/item/ModItems.java
+++ src/main/java/com/Infinity/Nexus/item/ModItems.java
@@ -1,8 +1,10 @@
 package com.Infinity.Nexus.item;
 
 import com.Infinity.Nexus.InfinityNexus;
+import com.Infinity.Nexus.Tab.ModTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,9 +14,69 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, InfinityNexus.MOD_ID);
 
-    public static final RegistryObject<Item> MACHINE = ITEMS.register("machine",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    //Custom
 
+    //Wires
+    public static final RegistryObject<Item> COPPER_WIRE = ITEMS.register("copper_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> ALUMINUM_WIRE = ITEMS.register("aluminum_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> IRON_WIRE = ITEMS.register("iron_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> GOLD_WIRE = ITEMS.register("gold_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> STEEL_WIRE = ITEMS.register("steel_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> MAGIC_WIRE = ITEMS.register("magic_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> INDUSTRIAL_WIRE = ITEMS.register("industrial_wire",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.EPIC)));
+
+    //Screws
+    public static final RegistryObject<Item> COPPER_SCREW = ITEMS.register("copper_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> ALUMINUM_SCREW = ITEMS.register("aluminum_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> IRON_SCREW = ITEMS.register("iron_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> STEEL_SCREW = ITEMS.register("steel_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> MAGIC_SCREW = ITEMS.register("magic_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> INDUSTRIAL_SCREW = ITEMS.register("industrial_screw",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.EPIC)));
+
+    //Bars
+    public static final RegistryObject<Item> COPPER_ROD = ITEMS.register("copper_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> ALUMINUM_ROD = ITEMS.register("aluminum_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> IRON_ROD = ITEMS.register("iron_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> GOLD_ROD = ITEMS.register("gold_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> STEEL_ROD = ITEMS.register("steel_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> MAGIC_ROD = ITEMS.register("magic_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> INDUSTRIAL_ROD = ITEMS.register("industrial_rod",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.EPIC)));
+
+    //Sheets
+    public static final RegistryObject<Item> COPPER_SHEET = ITEMS.register("copper_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> ALUMINUM_SHEET = ITEMS.register("aluminum_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.COMMON)));
+    public static final RegistryObject<Item> IRON_SHEET = ITEMS.register("iron_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> GOLD_SHEET = ITEMS.register("gold_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> STEEL_SHEET = ITEMS.register("steel_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> MAGIC_SHEET = ITEMS.register("magic_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> INDUSTRIAL_SHEET = ITEMS.register("industrial_sheet",
+            () -> new Item(new Item.Properties().tab(ModTabs.INFINITY_NEXUS).rarity(Rarity.EPIC)));
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
